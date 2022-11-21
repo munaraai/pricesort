f31d613d7fac7c5db1d9747f9bded9b66aac68f2
mona raai <monaraai@yahoo.com>
2022-11-21 20:30:17 +0300

    U

---
+++ src/price/Lowtohigh.java
@@ -0,0 +1,51 @@
+package price;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Lowtohigh{
+	public WebDriver driver;
+	@BeforeTest
+	public void login() {
+		WebDriverManager.chromedriver().setup();
+		driver= new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	@Test
+	public void sort() throws InterruptedException{
+		driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select")).click();
+		driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[3]")).click();
+		List<WebElement> thePricesList = driver.findElements(By.className("inventory_item_price"));
+		List<Double> newEditedList = new ArrayList<>();
+		for (int i = 0; i < thePricesList.size(); i++) {
+			String price = thePricesList.get(i).getText();
+			String editedPrice = price.replace("$", " ");
+			double val = Double.parseDouble(editedPrice.trim());
+			newEditedList.add(val);
+		}
+		for (int k = 0; k < newEditedList.size(); k++) {
+			boolean checkProcess = newEditedList.get(0) < newEditedList.get(newEditedList.size() - 1);
+			Assert.assertEquals(checkProcess, true);
+		}
+	}}
